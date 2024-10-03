07d9aeb929925916d3341d2a159ab5ee5afa39be
Wesley Tsai <wesxtsai@gmail.com>
2024-10-03 00:29:49 -0500

    Revert "push help"
    This reverts commit c6bff47d149361e4c3bd3bf28d85f69850ebebb7.
    
    help

--- woshicat-app/middleware.ts
+++ woshicat-app/middleware.ts
@@ -1,8 +1,6 @@
 // middleware.ts
 import { NextRequest, NextResponse } from 'next/server';
 
-const excludedPaths = ['/coming-soon'];
-
 export default function setCSPHeader(
     request: NextRequest
 ) {
@@ -12,7 +10,9 @@ export default function setCSPHeader(
     // Modify CSP header to include the nonce
     const cspHeader = `
         default-src 'self';
-        script-src 'self' 'nonce-${nonce}' 'unsafe-eval';
+        script-src 'self' 'nonce-${nonce}' 'strict-dynamic' ${
+            process.env.NODE_ENV === "production" ? "" : `'unsafe-eval'`
+          };
         style-src 'self' 'unsafe-inline';
         img-src 'self' cdn.shopify.com instagram.com cdninstagram.com blob: data:;
         frame-src 'self' cdn.shopify.com blob: data:;
