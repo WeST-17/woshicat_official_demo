602e8269ba5b9ff1aaf1e9907bd3d2dc8db2aee2
Wesley Tsai <admin@woshicat.com>
2024-10-09 19:05:19 -0500

    global error update

--- woshicat-app/app/global-error.tsx
+++ woshicat-app/app/global-error.tsx
@@ -1,6 +1,5 @@
-'use client'
-import Link from "next/link"
-import { Suspense } from "react"
+'use client';
+import Link from "next/link";
  
 export default function GlobalError({
   error,
@@ -13,11 +12,8 @@ export default function GlobalError({
     <html>
       <body>
         <div className="w-screen h-[100vh] flex flex-col justify-center items-center">
-        <Suspense fallback={<div>Loading...</div>}>
-          {/* Your code using useSearchParams() */}
-          <h2>Something went wrong!</h2>
+          <h2 className="mb-4">Something went wrong!</h2>
           <Link href={'/'} className="bg-black/50 text-white p-2 hover:bg-black transition duration-250">Back to Home</Link>
-        </Suspense>
         </div>
       </body>
     </html>
