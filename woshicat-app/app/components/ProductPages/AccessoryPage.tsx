786b3d642cb6fbfff75542ce8d751dda9e15b7a6
Wesley Tsai <admin@woshicat.com>
2024-10-12 01:54:27 -0500

    adjusted carousel photos at the bottom

--- woshicat-app/app/components/ProductPages/AccessoryPage.tsx
+++ woshicat-app/app/components/ProductPages/AccessoryPage.tsx
@@ -119,7 +119,7 @@ const AccessoryCard: React.FC<Handle> = ({ handle }) => {
                             className="flex"
                             placeholder={undefined}
                             navigation={({ setActiveIndex, activeIndex, length }) => (
-                                <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+                                <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2 max-sm:hidden">
                                     {new Array(length).fill("").map((_, i) => (
                                         <span
                                             key={i}
