f625cd8c04673a83e3a18c3458c2e55499408626
Wesley Tsai <admin@woshicat.com>
2024-10-18 19:25:26 -0500

    updated link for comic ep 1

--- woshicat-app/app/yoyo-friends/comic.ts
+++ woshicat-app/app/yoyo-friends/comic.ts
@@ -3,7 +3,7 @@ const ComicEps = [
         title: 'Morning Routine - Yoyo',
         date: 'October 2024',
         folder: 'comic-e1-oct-2024',
-        link: 'https://www.instagram.com/woshicatofficial',
+        link: 'https://www.instagram.com/p/DBRM4iMJFga/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==',
         dateID: 'oct-24',
         nextDate: '',
         prevDate: '',
