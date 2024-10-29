38661b7bc6ead7c06170a839cca8ad3275e81cbd
Wesley Tsai <admin@woshicat.com>
2024-10-28 23:41:08 -0500

    comic 3 update!

--- woshicat-app/app/yoyo-friends/comic.ts
+++ woshicat-app/app/yoyo-friends/comic.ts
@@ -15,9 +15,19 @@ const ComicEps = [
         folder: 'comic-e2-oct-2024',
         link: 'https://www.instagram.com/p/DBhYJ8ByDwg/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==',
         epID: 'ep-2-packed-sardines',
-        nextID: '',
+        nextID: 'ep-3-distracted',
         prevID: 'ep-1-weekday-mournings',
         thumbnail: 'https://res.cloudinary.com/woshicat-official/image/upload/c_thumb,w_200,g_face/v1729805245/E2P4V3_1_aop8jl.png'
+    },
+    {
+        title: `Ep.3 Distracted - Yoyo`,
+        date: 'Oct 28, 2024',
+        folder: 'comic-e3-oct-2024',
+        link: 'https://www.instagram.com/p/DBrPpqquf0c/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==',
+        epID: 'ep-3-distracted',
+        nextID: '',
+        prevID: 'ep-2-packed-sardines',
+        thumbnail: 'https://res.cloudinary.com/woshicat-official/image/upload/c_thumb,w_200,g_face/v1730174115/e3p4_bttdgm.png'
     },
 ];
 
