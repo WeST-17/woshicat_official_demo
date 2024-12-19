b2038fef522c7c4ca09568b5f57db6af6d38ef41
Wesley Tsai <admin@woshicat.com>
2024-12-18 21:18:39 -0600

    adjusted spacing on banner

--- woshicat-app/app/components/autoCarousel.tsx
+++ woshicat-app/app/components/autoCarousel.tsx
@@ -23,7 +23,7 @@ const AutoCarousel: React.FC<CarouselProps> = ({ messages }) => {
 
     return (
         <>
-            <div className="relative auto-carousel w-full h-8 flex justify-center items-center bg-orange-100 z-[2000]">
+            <div className="relative auto-carousel w-full h-20 md:h-8 flex justify-center items-center bg-orange-100 z-[2000]">
                 {messages.map((message, index) => (
                     <div className={`absolute transition-opacity duration-500 ${current === index ? 'opacity-100' : 'opacity-0'}`} key={index}>
                         {message}
