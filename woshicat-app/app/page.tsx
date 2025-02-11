75793c19d3ef07d0ab1d20a05a93b68fe2570f82
Wesley Tsai <admin@woshicat.com>
2025-02-11 13:57:17 -0600

    text promo change to buy

--- woshicat-app/app/page.tsx
+++ woshicat-app/app/page.tsx
@@ -135,7 +135,7 @@ const Home = () => {
         <EmailList />
         
       </div>
-      <Popup PromoLink="/collections/metro-daydreams" imgSrc="/media/homepage/woshi-hermanpark-05.jpg" imgAlt="Two people sitting on a window ledge in a park" promoDesc="Get one Metro Daydreams shirt, get one 50% off at checkout with code:" code="YOYOLOVE"/>
+      <Popup PromoLink="/collections/metro-daydreams" imgSrc="/media/homepage/woshi-hermanpark-05.jpg" imgAlt="Two people sitting on a window ledge in a park" promoDesc="Buy one Metro Daydreams shirt, get one 50% off at checkout with code:" code="YOYOLOVE"/>
     </main>
     </Transition>
     <TransitionSlide />
