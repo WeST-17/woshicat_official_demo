7cbc8ed5e63caad27212fb5cc0d3201754493d61
Wesley Tsai <admin@woshicat.com>
2025-02-24 16:56:55 -0600

    pushed yoy loading to all pages

--- woshicat-app/app/collections/lunar-new-year/[slug]/page.tsx
+++ woshicat-app/app/collections/lunar-new-year/[slug]/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { usePathname } from 'next/navigation';
 import SingleProductCard from '@/app/components/ProductPages/SingleProduct';
 import AccessoryCard from '@/app/components/ProductPages/AccessoryPage';
+import TransitionSlide from '@/app/components/transitionWipe';
 
 const ProductDetailPage = () => {
     const pathname = usePathname(); // get pathname: '/apparel/[handle]
@@ -12,9 +13,12 @@ const ProductDetailPage = () => {
     const isAccessory: boolean = handle.includes('sticker') || handle.includes('pin') || handle.includes('bundle');
     
     return (
+      <>
       <div>
        {!isAccessory ? <SingleProductCard handle={handle} /> : <AccessoryCard handle={handle} />} 
       </div>
+      <TransitionSlide />
+      </>
     );
 }
 
