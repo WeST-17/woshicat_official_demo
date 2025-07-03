e15979cbfa2a298fe602aac45be9ef8d01659f3b
woshicat <admin@woshicat.com>
2025-07-03 13:54:00 -0500

    resolved error with files

--- woshicat-app/app/yoyo-friends/[slug]/layout.tsx
+++ woshicat-app/app/yoyo-friends/[slug]/layout.tsx
@@ -1,7 +1,7 @@
 
 import { Suspense } from "react";
-import TransitionSlide from "@/app/components/transitionWipe";
-import Loader from "@/app/components/LoadingScreen";
+import TransitionSlide from "@/app/components/transitions-navigation/transitionWipe";
+import Loader from "@/app/components/transitions-navigation/LoadingScreen";
 
 export default function ComicLayout({
     children,
