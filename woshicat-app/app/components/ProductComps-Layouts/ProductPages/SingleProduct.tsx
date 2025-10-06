64903e439cba86b0b6642ec727fe59c7fbdc9fb0
woshicat <admin@woshicat.com>
2025-10-06 15:10:09 -0500

    oops wrong div

--- woshicat-app/app/components/ProductComps-Layouts/ProductPages/SingleProduct.tsx
+++ woshicat-app/app/components/ProductComps-Layouts/ProductPages/SingleProduct.tsx
@@ -443,8 +443,8 @@ const SingleProductCard: React.FC<Handle> = ({ handle }) => {
             <div className='h-fit w-full text-lg mx-auto flex items-start'>
                 <ScrollingCarousel addClass='' numPerSlide={3.2} mobileSlide={1.2} length={10} type={'recommendations'}>
                 {recommendations.map((product: any) => (
-                    <div className={`w-full h-full px-1`} key={product.id}>
-                        <div className={`relative text-center h-full bg-white/80`} 
+                    <div className={`w-full h-full px-1 bg-white/80`} key={product.id}>
+                        <div className={`relative text-center h-full`} 
                             key={product.id}
                         >
                             <div className={`z-[1000] rounded-md absolute top-0 right-0 p-2 m-1 text-white bg-red-800 pointer-events-none ${!product.available ? '' : 'hidden'}`}>Sold Out!</div>
