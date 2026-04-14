eaef6697676b82cd3e37ba63c107a1c467312c69
woshicat <admin@woshicat.com>
2026-04-13 23:54:03 -0500

    updating meta pixel library to custom add consent functionality

--- woshicat-app/app/components/cart/cartDisplay.tsx
+++ woshicat-app/app/components/cart/cartDisplay.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-// import { motion } from "framer-motion";
+// import { motion } from "framer-motion"; 
 import { useEffect, useRef } from "react";
 import Link from "next/link";
 import ProgressBar from "./progressBar";
